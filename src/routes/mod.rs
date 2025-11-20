4a60cf9e4fd6a211f5c3c4d107a8f804fbba2f1f
Tom Hacohen <tom@stosb.com>
2025-11-20 14:01:28 -0800

    snap

--- src/routes/mod.rs
+++
@@ -1,9 +0,0 @@
-use axum::Router;
-
-pub mod health;
-pub mod kv;
-pub mod ratelimit;
-
-pub fn router() -> Router {
-    Router::new().merge(health::router()).merge(kv::router()).merge(ratelimit::router())
-}
