b275905f10733230ca455733efd931966ddb256d
Tom Hacohen <tom@stosb.com>
2025-11-20 15:28:30 -0800

    snap

--- coyote/src/v1/endpoints/mod.rs
+++ coyote/src/v1/endpoints/mod.rs
@@ -1,4 +1,11 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
+use aide::axum::ApiRouter;
+
+use crate::AppState;
 
 pub mod health;
+
+pub fn router() -> ApiRouter<AppState> {
+    ApiRouter::new().merge(health::router())
+}
