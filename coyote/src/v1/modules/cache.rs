f6696e13ba66716c8ab5d20736d488b8301d56db
Tom Hacohen <tom@stosb.com>
2025-12-07 17:58:38 -0800

    snap

--- coyote/src/v1/modules/cache.rs
+++ coyote/src/v1/modules/cache.rs
@@ -1,6 +1,87 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
 
-//! Cache module - re-exports from endpoints for now
+//! Cache module.
+//!
+//! The idea of having a separate cache module is that we can aggressively evict from this one when
+//! under memory pressure, which we don't want to do with kv store (which can't be lost!). So cache
+//! is really for caching things, and not a kv store. That's why they should maybe be different.
+//! So for example we can configure eviction policies like: swap, drop, and behaviors like lru,
+//! whatever.
+//!
+//! FIXME:
+//! * Potentially we could merge it with KV and just with the "group configuration" behavior we can
+//!   define the cache behavior. So we don't actually need a different backend?
+//!   * Though even if we do that, maybe cache should be an alias for kv with a default base
+//!     configuration?
+//! * If we end up making them separate: this can potentially reuse code from kv-store?
 
-pub use crate::v1::endpoints::cache::{worker, CacheStore};
+use dashmap::DashMap;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use validator::Validate;
+
+use crate::{
+    core::types::EntityKey,
+    error::{Error, HttpError, Result},
+    AppState,
+};
+
+#[derive(Clone)]
+pub struct CacheStore {
+    pub(crate) store: Arc<DashMap<String, CacheModel>>,
+}
+
+impl Default for CacheStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl CacheStore {
+    pub fn new() -> Self {
+        Self {
+            store: Arc::new(DashMap::new()),
+        }
+    }
+
+    pub fn set(&self, key: String, model: CacheModel) {
+        self.store.insert(key, model);
+    }
+
+    pub fn get(&self, key: &str) -> Result<CacheModel> {
+        self.store
+            .get(key)
+            .map(|entry| entry.value().clone())
+            .ok_or_else(|| Error::http(HttpError::not_found(None, None)))
+    }
+
+    pub fn delete(&self, key: &str) -> bool {
+        self.store.remove(key).is_some()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Validate, JsonSchema)]
+pub struct CacheModel {
+    #[validate]
+    pub key: EntityKey,
+
+    // FIXME: should be datetime
+    /// Time of expiry
+    pub expires_at: u64,
+
+    // FIXME: change to Bytes
+    pub value: String,
+}
+
+/// This is the worker function for this module, it does background cleanup and accounting.
+pub async fn worker(_state: AppState) -> Result<()> {
+    loop {
+        if crate::is_shutting_down() {
+            break;
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    }
+    Ok(())
+}
