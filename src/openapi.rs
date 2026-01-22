b212089975724ca6ad8d984b8e55eee64b0723c1
Jonas Platte <jplatte@svix.com>
2026-01-22 14:54:24 +0100

    Clean up weird schemas generated by schemars v1.x

--- src/openapi.rs
+++ src/openapi.rs
@@ -149,12 +149,45 @@ fn add_idempotency_to_post(openapi: &mut OpenApi) {
 /// circumstances not referenced. At the moment these are struct schemas used
 /// by query parameters and path placeholders.
 fn remove_unneeded_schemas(openapi: &mut OpenApi) {
-    if let Some(components) = &mut openapi.components {
-        components.schemas.retain(|name, _| {
-            !(name.ends_with("Path")
-                || name.ends_with("QueryParams")
-                || name.starts_with("Pagination"))
-        });
+    let Some(components) = &mut openapi.components else {
+        return;
+    };
+
+    components.schemas.retain(|name, _| {
+        !(name.ends_with("Path") || name.ends_with("QueryParams") || name.starts_with("Pagination"))
+    });
+}
+
+fn flatten_weird_nested_ref(openapi: &mut OpenApi) {
+    let Some(components) = &mut openapi.components else {
+        return;
+    };
+
+    for schema in components.schemas.values_mut() {
+        let Some(schema) = schema.json_schema.as_object_mut() else {
+            continue;
+        };
+        let Some(props) = schema.get_mut("properties") else {
+            continue;
+        };
+        let Some(props) = props.as_object_mut() else {
+            continue;
+        };
+
+        for prop in props.values_mut() {
+            if let Some(schema) = prop.as_object_mut()
+                && !schema.contains_key("$ref")
+                && let Some(all_of) = schema.get_mut("allOf")
+                && let Some(all_of) = all_of.as_array_mut()
+                && all_of.len() == 1
+                && let Some(sub) = all_of[0].as_object_mut()
+                && sub.len() == 1
+                && let Some((k, v)) = sub.remove_entry("$ref")
+            {
+                schema.insert(k, v);
+                schema.remove("allOf");
+            }
+        }
     }
 }
 
@@ -175,7 +208,11 @@ pub fn add_security_scheme(
 /// Applies a list of hacks to the finished OpenAPI spec to make it usable with
 /// our tooling.
 pub fn postprocess_spec(openapi: &mut OpenApi) {
-    let hacks = [add_idempotency_to_post, remove_unneeded_schemas];
+    let hacks = [
+        add_idempotency_to_post,
+        flatten_weird_nested_ref,
+        remove_unneeded_schemas,
+    ];
 
     for hack in hacks {
         hack(openapi);
