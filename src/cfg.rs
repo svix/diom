33ee9ab554ea48bbfe917f8068b6abcf233eb7f3
James Lucas <102536031+svix-james@users.noreply.github.com>
2026-01-23 09:31:03 -0800

    Some db scaffolding (#10)
    Maybe this is helpful, maybe just clumsy, but adding some
    configuration for multiple databases, which are tentatively
    differentiated as "persistent" and "ephemeral". Oh, and don't
    forget the "management" db, which will store config info.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,10 +1,13 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
 
-use std::{fmt, net::SocketAddr, sync::Arc};
+use std::{fmt, marker::PhantomData, net::SocketAddr, path::PathBuf, sync::Arc};
 
+use crate::error::Result;
 use anyhow::Context;
+use fjall::Database;
 use serde::Deserialize;
+use serde_with::with_prefix;
 use tracing::Level;
 
 use crate::core::security::JwtSigningConfig;
@@ -13,13 +16,81 @@ const DEFAULTS: &str = include_str!("../config.default.toml");
 
 pub type Configuration = Arc<ConfigurationInner>;
 
+with_prefix!(ephemeral_db "ephemeral_db_");
+with_prefix!(persistent_db "persistent_db_");
+with_prefix!(management_db "management_db_");
+
+pub trait StorageType {}
+
+#[derive(Debug, Default)]
+pub struct Ephemeral {}
+impl StorageType for Ephemeral {}
+
+#[derive(Debug, Default)]
+pub struct Persistent {}
+impl StorageType for Persistent {}
+
+#[derive(Debug, Default)]
+pub struct Management {}
+impl StorageType for Management {}
+
+#[derive(Clone, Debug, Default, Deserialize)]
+pub struct DatabaseConfig<S: StorageType> {
+    pub path: String,
+    #[serde(default)]
+    pub filename: Option<String>,
+    #[serde(skip_serializing, default)]
+    pub _phantom: PhantomData<S>,
+}
+
+impl<S: StorageType> DatabaseConfig<S> {
+    fn database(dir: &str, file: &str) -> Result<Database> {
+        let mut path = PathBuf::from(dir);
+        path.push(file);
+        fjall::Database::builder(path).open().map_err(|e| e.into())
+    }
+}
+
+impl DatabaseConfig<Ephemeral> {
+    pub fn persistent(db_config: Arc<DatabaseConfig<Persistent>>) -> Result<Database> {
+        Self::database(
+            &db_config.path,
+            db_config.filename.as_deref().unwrap_or("fjall_persistent"),
+        )
+    }
+}
+
+impl DatabaseConfig<Persistent> {
+    pub fn ephemeral(db_config: Arc<DatabaseConfig<Ephemeral>>) -> Result<Database> {
+        Self::database(
+            &db_config.path,
+            db_config.filename.as_deref().unwrap_or("fjall_ephemeral"),
+        )
+    }
+}
+
+impl DatabaseConfig<Management> {
+    pub fn management(db_config: Arc<DatabaseConfig<Management>>) -> Result<Database> {
+        Self::database(
+            &db_config.path,
+            db_config.filename.as_deref().unwrap_or("fjall_management"),
+        )
+    }
+}
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct ConfigurationInner {
     /// The address to listen on
     pub listen_address: SocketAddr,
 
-    /// The directory for on-disk storage.
-    pub db_directory: String,
+    #[serde(flatten, with = "management_db")]
+    pub management_db_config: Arc<DatabaseConfig<Management>>,
+
+    #[serde(flatten, with = "persistent_db")]
+    pub peristent_db_config: Arc<DatabaseConfig<Persistent>>,
+
+    #[serde(flatten, with = "ephemeral_db")]
+    pub ephemeral_db_config: Arc<DatabaseConfig<Ephemeral>>,
 
     /// Contains the secret and algorithm for signing JWTs
     #[serde(flatten)]
@@ -119,4 +190,50 @@ pub fn load() -> anyhow::Result<Arc<ConfigurationInner>> {
         .context("failed to extract configuration")?;
 
     Ok(Arc::from(config))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Deserialize)]
+    struct TestConfig {
+        #[serde(flatten, with = "persistent_db")]
+        pub persistent_db_config: Arc<DatabaseConfig<Persistent>>,
+
+        #[serde(flatten, with = "ephemeral_db")]
+        pub ephemeral_db_config: Arc<DatabaseConfig<Ephemeral>>,
+
+        #[serde(flatten, with = "management_db")]
+        pub management_db_config: Arc<DatabaseConfig<Management>>,
+    }
+
+    #[test]
+    fn test_db() {
+        let raw_config = r#"
+ephemeral_db_path="/1"
+ephemeral_db_filename="test1"
+persistent_db_path="/2"
+management_db_path="/3"
+"#;
+
+        let config = config::Config::builder()
+            .add_source(config::File::from_str(raw_config, config::FileFormat::Toml))
+            .build()
+            .unwrap();
+
+        let db_config = config.clone().try_deserialize::<TestConfig>().unwrap();
+
+        assert_eq!(db_config.ephemeral_db_config.path, "/1".to_string());
+        assert_eq!(
+            db_config.ephemeral_db_config.filename,
+            Some("test1".to_string())
+        );
+
+        assert_eq!(db_config.persistent_db_config.path, "/2".to_string());
+        assert!(db_config.persistent_db_config.filename.is_none());
+
+        assert_eq!(db_config.management_db_config.path, "/3".to_string());
+        assert!(db_config.management_db_config.filename.is_none());
+    }
 }
