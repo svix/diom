e6eee65d518e1995be26795835d6ee0fff1a810b
Jonas Platte <jplatte@svix.com>
2026-01-26 11:48:16 +0100

    Sort dependencies

--- lint.sh
+++ lint.sh
@@ -1,2 +1,3 @@
 cargo +nightly clippy --fix --allow-dirty --all-features --all-targets
 cargo +nightly fmt
+cargo sort --workspace -o package,lib,bin,features,dependencies,dev-dependencies,lints,workspace
