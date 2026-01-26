82ac9aea1bb7261e60440c7d8b2794c9d76611da
Jonas Platte <jplatte@svix.com>
2026-01-26 15:33:37 +0100

    Replace lint.sh by justfile

--- lint.sh
+++
@@ -1,8 +0,0 @@
-cargo +nightly clippy \
-    --fix \
-    --allow-dirty \
-    --workspace \
-    --all-features \
-    --all-targets
-cargo +nightly fmt
-cargo sort --workspace -o package,lib,bin,features,dependencies,dev-dependencies,lints,workspace
