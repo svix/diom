1416c48c8ad970ef485be58678061edb24569441
Jonas Platte <jplatte@svix.com>
2026-01-26 17:26:03 +0100

    Update openapi-codegen, openapi.json, generated code

---
+++ clients/rust/src/models/append_to_stream_out.rs
@@ -0,0 +1,15 @@
+// this file is @generated
+use serde::{Deserialize, Serialize};
+
+#[non_exhaustive]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AppendToStreamOut {
+    #[serde(rename = "msgIds")]
+    pub msg_ids: Vec<u64>,
+}
+
+impl AppendToStreamOut {
+    pub fn new(msg_ids: Vec<u64>) -> Self {
+        Self { msg_ids }
+    }
+}
--- clients/rust/src/models/mod.rs
+++ clients/rust/src/models/mod.rs
@@ -1,6 +1,8 @@
 // this file is @generated
 #![allow(clippy::too_many_arguments)]
 
+mod append_to_stream_in;
+mod append_to_stream_out;
 mod cache_delete_in;
 mod cache_delete_out;
 mod cache_get_in;
@@ -15,6 +17,7 @@ mod kv_get_in;
 mod kv_get_out;
 mod kv_set_in;
 mod kv_set_out;
+mod msg_in;
 mod operation_behavior;
 mod queue_ack_in;
 mod queue_ack_out;
@@ -38,16 +41,18 @@ mod rate_limiter_get_remaining_in;
 mod rate_limiter_get_remaining_out;
 
 pub use self::{
+    append_to_stream_in::AppendToStreamIn, append_to_stream_out::AppendToStreamOut,
     cache_delete_in::CacheDeleteIn, cache_delete_out::CacheDeleteOut, cache_get_in::CacheGetIn,
     cache_get_out::CacheGetOut, cache_set_in::CacheSetIn, cache_set_out::CacheSetOut,
     create_stream_in::CreateStreamIn, create_stream_out::CreateStreamOut, kv_delete_in::KvDeleteIn,
     kv_delete_out::KvDeleteOut, kv_get_in::KvGetIn, kv_get_out::KvGetOut, kv_set_in::KvSetIn,
-    kv_set_out::KvSetOut, operation_behavior::OperationBehavior, queue_ack_in::QueueAckIn,
-    queue_ack_out::QueueAckOut, queue_message::QueueMessage, queue_nack_in::QueueNackIn,
-    queue_nack_out::QueueNackOut, queue_purge_in::QueuePurgeIn, queue_purge_out::QueuePurgeOut,
-    queue_receive_in::QueueReceiveIn, queue_receive_out::QueueReceiveOut,
-    queue_reject_in::QueueRejectIn, queue_reject_out::QueueRejectOut, queue_send_in::QueueSendIn,
-    queue_send_out::QueueSendOut, queue_stats_in::QueueStatsIn, queue_stats_out::QueueStatsOut,
+    kv_set_out::KvSetOut, msg_in::MsgIn, operation_behavior::OperationBehavior,
+    queue_ack_in::QueueAckIn, queue_ack_out::QueueAckOut, queue_message::QueueMessage,
+    queue_nack_in::QueueNackIn, queue_nack_out::QueueNackOut, queue_purge_in::QueuePurgeIn,
+    queue_purge_out::QueuePurgeOut, queue_receive_in::QueueReceiveIn,
+    queue_receive_out::QueueReceiveOut, queue_reject_in::QueueRejectIn,
+    queue_reject_out::QueueRejectOut, queue_send_in::QueueSendIn, queue_send_out::QueueSendOut,
+    queue_stats_in::QueueStatsIn, queue_stats_out::QueueStatsOut,
     rate_limiter_check_in::RateLimiterCheckIn, rate_limiter_check_out::RateLimiterCheckOut,
     rate_limiter_config::RateLimiterConfig,
     rate_limiter_get_remaining_in::RateLimiterGetRemainingIn,
