203ae44468e9840dfea1ff0e6ce94d19166a001a
Gabriel <gabriel@svix.com>
2026-01-29 10:10:32 -0500

    Update SDK

--- clients/cli/src/cmds/api/mod.rs
+++ clients/cli/src/cmds/api/mod.rs
@@ -1,11 +1,9 @@
 // this file is @generated
 mod cache;
 mod kv;
-mod queue;
 mod rate_limiter;
 mod stream;
 
 pub(crate) use self::{
-    cache::CacheArgs, kv::KvArgs, queue::QueueArgs, rate_limiter::RateLimiterArgs,
-    stream::StreamArgs,
+    cache::CacheArgs, kv::KvArgs, rate_limiter::RateLimiterArgs, stream::StreamArgs,
 };
--- clients/rust/src/api/stream.rs
+++ clients/rust/src/api/stream.rs
@@ -11,6 +11,16 @@ pub struct StreamAppendOptions {
     pub idempotency_key: Option<String>,
 }
 
+#[derive(Default)]
+pub struct StreamFetchLockingOptions {
+    pub idempotency_key: Option<String>,
+}
+
+#[derive(Default)]
+pub struct StreamAckOptions {
+    pub idempotency_key: Option<String>,
+}
+
 pub struct Stream<'a> {
     cfg: &'a Configuration,
 }
@@ -46,6 +56,35 @@ impl<'a> Stream<'a> {
         crate::request::Request::new(http::Method::POST, "/api/v1/stream/append")
             .with_optional_header_param("idempotency-key", idempotency_key)
             .with_body_param(append_to_stream_in)
+            .execute(self.cfg)
+            .await
+    }
+
+    /// Fetches messages from the stream, locking over the consumer group.
+    ///
+    /// This call prevents other consumers within the same consumer group from reading from the stream
+    /// until either the visibility timeout expires, or the last message in the batch is acknowledged.
+    pub async fn fetch_locking(
+        &self,
+        fetch_from_stream_in: FetchFromStreamIn,
+        options: Option<StreamFetchLockingOptions>,
+    ) -> Result<FetchFromStreamOut> {
+        let StreamFetchLockingOptions { idempotency_key } = options.unwrap_or_default();
+
+        crate::request::Request::new(http::Method::POST, "/api/v1/stream/fetch-locking")
+            .with_optional_header_param("idempotency-key", idempotency_key)
+            .with_body_param(fetch_from_stream_in)
+            .execute(self.cfg)
+            .await
+    }
+
+    /// Acks the messages for the consumer group, allowing more messages to be consumed.
+    pub async fn ack(&self, ack_in: AckIn, options: Option<StreamAckOptions>) -> Result<AckOut> {
+        let StreamAckOptions { idempotency_key } = options.unwrap_or_default();
+
+        crate::request::Request::new(http::Method::POST, "/api/v1/stream/ack")
+            .with_optional_header_param("idempotency-key", idempotency_key)
+            .with_body_param(ack_in)
             .execute(self.cfg)
             .await
     }
