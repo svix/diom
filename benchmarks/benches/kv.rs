ae36054e274d30e224ad8f96275761c1f14afc5b
James Lucas <svix-james@users.noreply.github.com>
2026-02-05 18:13:28 -0600

    Add cluster testing to benchmarks

--- benchmarks/benches/kv.rs
+++ benchmarks/benches/kv.rs
@@ -1,15 +1,20 @@
+use coyote_benchmarks::{BenchmarkContext, setup_cluster, setup_single_server};
 use coyote_client::models::{KvDeleteIn, KvGetIn, KvSetIn};
-use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
+use criterion::{
+    BatchSize, BenchmarkGroup, Criterion, criterion_group, criterion_main, measurement::Measurement,
+};
 use rand::{
-    Rng,
+    Rng, SeedableRng,
     distr::{Alphanumeric, SampleString},
+    rngs::StdRng,
 };
 
-fn bench_kv(c: &mut Criterion) {
-    let ctx = coyote_benchmarks::setup_server_simple();
+fn bench_kv<'a, M: Measurement>(ctx: BenchmarkContext, group: &mut BenchmarkGroup<'a, M>) {
     let client = ctx.client;
 
-    let mut rng = rand::rng();
+    group.measurement_time(std::time::Duration::from_secs(15));
+
+    let mut rng = StdRng::seed_from_u64(0);
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
@@ -19,7 +24,7 @@ fn bench_kv(c: &mut Criterion) {
     let mut test_val = vec![0u8; 256];
     rng.fill(&mut test_val[..]);
 
-    c.bench_function("kv_set_fixed", |b| {
+    group.bench_function("kv_set_fixed", |b| {
         b.iter_batched(
             || (test_key.clone(), test_val.clone()),
             |(key, val)| {
@@ -33,7 +38,7 @@ fn bench_kv(c: &mut Criterion) {
         )
     });
 
-    c.bench_function("kv_set_random_small", |b| {
+    group.bench_function("kv_set_random_small", |b| {
         b.iter_batched(
             || {
                 let k = Alphanumeric.sample_string(&mut rng, 16);
@@ -52,7 +57,7 @@ fn bench_kv(c: &mut Criterion) {
         )
     });
 
-    c.bench_function("kv_set_random_medium_value", |b| {
+    group.bench_function("kv_set_random_medium_value", |b| {
         b.iter_batched(
             || {
                 let k = Alphanumeric.sample_string(&mut rng, 16);
@@ -71,7 +76,7 @@ fn bench_kv(c: &mut Criterion) {
         )
     });
 
-    c.bench_function("kv_set_random_large_value", |b| {
+    group.bench_function("kv_set_random_large_value", |b| {
         b.iter_batched(
             || {
                 let k = Alphanumeric.sample_string(&mut rng, 16);
@@ -101,7 +106,7 @@ fn bench_kv(c: &mut Criterion) {
         .unwrap();
     });
 
-    c.bench_function("kv_get", |b| {
+    group.bench_function("kv_get", |b| {
         b.iter_batched(
             || test_key.clone(),
             |key| {
@@ -115,7 +120,7 @@ fn bench_kv(c: &mut Criterion) {
         )
     });
 
-    c.bench_function("kv_set_delete", |b| {
+    group.bench_function("kv_set_delete", |b| {
         b.iter_batched(
             || (test_key.clone(), test_key.clone(), test_val.clone()),
             |(key1, key2, test_val)| {
@@ -133,5 +138,17 @@ fn bench_kv(c: &mut Criterion) {
     });
 }
 
-criterion_group!(kv, bench_kv);
+fn bench_kv_single(c: &mut Criterion) {
+    let ctx = setup_single_server();
+    let mut group = c.benchmark_group("kv_single_server");
+    bench_kv(ctx, &mut group);
+}
+
+fn bench_kv_cluster(c: &mut Criterion) {
+    let ctx = setup_cluster(3);
+    let mut group = c.benchmark_group("kv_cluster");
+    bench_kv(ctx, &mut group);
+}
+
+criterion_group!(kv, bench_kv_single, bench_kv_cluster);
 criterion_main!(kv);
