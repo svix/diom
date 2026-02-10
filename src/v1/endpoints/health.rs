e31dde7a3fc208bd9f205b9f9f6c886405508b85
Jonas Platte <jplatte@svix.com>
2026-02-10 11:54:12 +0100

    Remove redundant HEAD health/ping and add operation ID
    HEAD is supported implicitly when GET is.

--- src/v1/endpoints/health.rs
+++ src/v1/endpoints/health.rs
@@ -1,7 +1,8 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
 
-use aide::axum::{ApiRouter, routing::get};
+use aide::axum::{ApiRouter, routing::get_with};
+use coyote_derive::aide_annotate;
 use coyote_proto::MsgPackOrJson;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -13,6 +14,8 @@ pub struct PingOut {
     pub ok: bool,
 }
 
+/// Verify the server is up and running.
+#[aide_annotate(op_id = "v1.health.ping")]
 async fn ping() -> Result<MsgPackOrJson<PingOut>> {
     Ok(MsgPackOrJson(PingOut { ok: true }))
 }
@@ -20,5 +23,5 @@ async fn ping() -> Result<MsgPackOrJson<PingOut>> {
 pub fn router() -> ApiRouter<AppState> {
     let tag = openapi_tag("Health");
 
-    ApiRouter::new().api_route_with("/health/ping", get(ping).head(ping), &tag)
+    ApiRouter::new().api_route_with("/health/ping", get_with(ping, ping_operation), &tag)
 }
