92dec1fe9831ccb11c3c1ec2b49e2212a6feb241
Jonas Platte <jplatte@svix.com>
2026-02-13 17:52:36 +0100

    Add OpenAPI linting (#121)

---
+++ clients/cli/src/cmds/api/health.rs
@@ -0,0 +1,33 @@
+// this file is @generated
+use clap::{Args, Subcommand};
+use diom_client::DiomClient;
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true, flatten_help = true)]
+pub struct HealthArgs {
+    #[command(subcommand)]
+    pub command: HealthCommands,
+}
+
+#[derive(Subcommand)]
+pub enum HealthCommands {
+    /// Verify the server is up and running.
+    Ping {},
+}
+
+impl HealthCommands {
+    pub async fn exec(
+        self,
+        client: &DiomClient,
+        color_mode: colored_json::ColorMode,
+    ) -> anyhow::Result<()> {
+        match self {
+            Self::Ping {} => {
+                let resp = client.health().ping().await?;
+                crate::json::print_json_output(&resp, color_mode)?;
+            }
+        }
+
+        Ok(())
+    }
+}
