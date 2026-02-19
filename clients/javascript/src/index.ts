a7a0cc58c72372f8d796aba0665f3740ae7c6f7e
Jonas Platte <jplatte@svix.com>
2026-02-19 14:21:46 +0100

    Add JS SDK index.ts

---
+++ clients/javascript/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./client";
+export { DiomOptions } from "./options";
+export { ApiException } from "./util";
+export { HTTPValidationError, HttpErrorOut, ValidationError } from "./HttpErrors";
