6e4aa869831fb49bb797818d2bfbba401d435585
Jonas Platte <jplatte@svix.com>
2026-02-20 18:18:20 +0100

    Replace config crate with custom env var parsing
    … for the main config file.

--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: MIT
 
 use std::{
-    fmt,
+    fmt, fs,
     net::SocketAddr,
     path::{Path, PathBuf},
+    str::FromStr,
     sync::Arc,
     time::Duration,
 };
 
-use anyhow::Context;
-use config::ConfigBuilder;
+use anyhow::{Context, anyhow};
 use fjall::Database;
 use serde::{Deserialize, de::DeserializeOwned};
 use tap::Pipe;
@@ -182,12 +182,21 @@ pub struct ConfigurationInner {
     pub cluster: ClusterConfiguration,
 }
 
-#[cfg(test)]
-/// make a ConfigurationInner for testing use
 impl Default for ConfigurationInner {
     fn default() -> Self {
         default_from_serde().unwrap()
     }
+}
+
+macro_rules! from_str_via_serde {
+    ($ty:ty) => {
+        impl FromStr for $ty {
+            type Err = serde::de::value::Error;
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                <Self as serde::Deserialize>::deserialize(serde::de::value::StrDeserializer::new(s))
+            }
+        }
+    };
 }
 
 #[derive(Clone, Debug, Default, Deserialize)]
@@ -199,6 +208,8 @@ pub enum LogLevel {
     Trace,
 }
 
+from_str_via_serde!(LogLevel);
+
 #[derive(Clone, Debug, Default, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum LogFormat {
@@ -206,6 +217,8 @@ pub enum LogFormat {
     Default,
     Json,
 }
+
+from_str_via_serde!(LogFormat);
 
 #[derive(Clone, Debug, Default, Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -215,6 +228,8 @@ pub enum Environment {
     Staging,
     Prod,
 }
+
+from_str_via_serde!(Environment);
 
 impl std::fmt::Display for Environment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -242,39 +257,172 @@ impl fmt::Display for LogLevel {
 }
 
 pub fn load(config_path: Option<&str>) -> anyhow::Result<Arc<ConfigurationInner>> {
-    let config = config::Config::builder()
-        .pipe(|config: ConfigBuilder<_>| {
-            if let Some(path) = config_path {
-                config.add_source(config::File::with_name(path))
-            } else {
-                config
-            }
-        })
-        .add_source(
-            config::Environment::with_prefix("DIOM")
-                .list_separator(",")
-                .separator(":")
-                .try_parsing(true)
-                .with_list_parse_key("cluster.seed_nodes"),
-        )
-        .build()?;
+    let config_toml = match config_path {
+        Some(path) => Some(fs::read_to_string(path).context("reading config file")?),
+        None => None,
+    };
+    load_toml(config_toml.as_deref())
+}
 
-    let config = config
-        .try_deserialize::<ConfigurationInner>()
-        .context("failed to extract configuration")?;
+fn load_toml(config_toml: Option<&str>) -> anyhow::Result<Arc<ConfigurationInner>> {
+    let mut config = match config_toml {
+        Some(config_toml) => toml::from_str(config_toml).context("parsing config file")?,
+        None => ConfigurationInner::default(),
+    };
+
+    macro_rules! env_overrides {
+        ( $( $field:ident: $env_var:literal ),* $(,)? ) => {
+            $(
+                if let Some(value) = env_var($env_var)? {
+                    *$field = value;
+                }
+            )*
+        };
+    }
+
+    let ConfigurationInner {
+        listen_address,
+        persistent_db:
+            DatabaseConfig {
+                path: persistent_db_path,
+                filename: persistent_db_filename,
+            },
+        ephemeral_db:
+            DatabaseConfig {
+                path: ephemeral_db_path,
+                filename: ephemeral_db_filename,
+            },
+        log_level,
+        log_format,
+        opentelemetry_address,
+        opentelemetry_metrics_use_http,
+        opentelemetry_metrics_period_seconds,
+        opentelemetry_sample_ratio,
+        opentelemetry_service_name,
+        environment,
+        cluster:
+            ClusterConfiguration {
+                listen_address: cluster_listen_address,
+                name: cluster_name,
+                snapshot_path: cluster_snapshot_path,
+                log_path: cluster_log_path,
+                secret: cluster_secret,
+                replication_request_timeout: cluster_replication_request_timeout,
+                discovery_request_timeout: cluster_discovery_request_timeout,
+                connection_timeout: cluster_connection_timeout,
+                heartbeat_interval_ms: cluster_heartbeat_interval_ms,
+                election_timeout_min_ms: cluster_election_timeout_min_ms,
+                election_timeout_max_ms: cluster_election_timeout_max_ms,
+                seed_nodes: cluster_seed_nodes,
+                auto_initialize: cluster_auto_initialize,
+                discovery_timeout: cluster_discovery_timeout,
+                startup_discovery_delay: cluster_startup_discovery_delay,
+            },
+    } = &mut config;
+
+    env_overrides!(
+        listen_address: "DIOM_LISTEN_ADDRESS",
+        persistent_db_path: "DIOM_PERSISTENT_DB_PATH",
+        ephemeral_db_path: "DIOM_EPHEMERAL_DB_PATH",
+        log_level: "DIOM_LOG_LEVEL",
+        log_format: "DIOM_LOG_FORMAT",
+        opentelemetry_metrics_use_http: "DIOM_OPENTELEMETRY_METRICS_USE_HTTP",
+        opentelemetry_metrics_period_seconds: "DIOM_OPENTELEMETRY_METRICS_PERIOD_SECONDS",
+        opentelemetry_service_name: "DIOM_OPENTELEMETRY_SERVICE_NAME",
+        environment: "DIOM_ENVIRONMENT",
+        cluster_listen_address: "DIOM_CLUSTER_LISTEN_ADDRESS",
+        cluster_name: "DIOM_CLUSTER_NAME",
+        cluster_snapshot_path: "DIOM_CLUSTER_SNAPSHOT_PATH",
+        cluster_log_path: "DIOM_CLUSTER_LOG_PATH",
+        cluster_heartbeat_interval_ms: "DIOM_CLUSTER_HEARTBEAT_INTERVAL_MS",
+        cluster_election_timeout_min_ms: "DIOM_CLUSTER_ELECTION_TIMEOUT_MIN_MS",
+        cluster_election_timeout_max_ms: "DIOM_CLUSTER_ELECTION_TIMEOUT_MAX_MS",
+        cluster_auto_initialize: "DIOM_CLUSTER_AUTO_INITIALIZE",
+    );
+
+    // Option fields not supported by the simple macro above.
+    if let Some(value) = env_var("DIOM_PERSISTENT_DB_FILENAME")? {
+        *persistent_db_filename = Some(value);
+    }
+    if let Some(value) = env_var("DIOM_EPHEMERAL_DB_FILENAME")? {
+        *ephemeral_db_filename = Some(value);
+    }
+    if let Some(value) = env_var("DIOM_OPENTELEMETRY_ADDRESS")? {
+        *opentelemetry_address = Some(value);
+    }
+    if let Some(value) = env_var("DIOM_OPENTELEMETRY_SAMPLE_RATIO")? {
+        *opentelemetry_sample_ratio = Some(value);
+    }
+    if let Some(value) = env_var("DIOM_CLUSTER_SECRET")? {
+        *cluster_secret = Some(value);
+    }
+
+    // Fields that require different parsing
+    if let Some(value) = env_var_comma_separated("DIOM_CLUSTER_SEED_NODES")? {
+        *cluster_seed_nodes = value;
+    }
+    if let Some(value) = env_var_ms("DIOM_CLUSTER_REPLICATION_REQUEST_TIMEOUT_MS")? {
+        *cluster_replication_request_timeout = value;
+    }
+    if let Some(value) = env_var_ms("DIOM_CLUSTER_DISCOVERY_REQUEST_TIMEOUT_MS")? {
+        *cluster_discovery_request_timeout = value;
+    }
+    if let Some(value) = env_var_ms("DIOM_CLUSTER_CONNECTION_TIMEOUT_MS")? {
+        *cluster_connection_timeout = value;
+    }
+    if let Some(value) = env_var_ms("DIOM_CLUSTER_DISCOVERY_TIMEOUT_MS")? {
+        *cluster_discovery_timeout = value;
+    }
+    if let Some(value) = env_var_ms("DIOM_CLUSTER_STARTUP_DISCOVERY_DELAY_MS")? {
+        *cluster_startup_discovery_delay = value;
+    };
 
     Ok(Arc::from(config))
 }
 
+fn env_var<T>(name: &'static str) -> anyhow::Result<Option<T>>
+where
+    T: FromStr<Err: fmt::Display>,
+{
+    env_var_parse(name, FromStr::from_str)
+}
+
+fn env_var_ms(name: &'static str) -> anyhow::Result<Option<Duration>> {
+    env_var::<u64>(name)?.map(Duration::from_millis).pipe(Ok)
+}
+
+fn env_var_comma_separated<T>(name: &'static str) -> anyhow::Result<Option<Vec<T>>>
+where
+    T: FromStr<Err: fmt::Display>,
+{
+    env_var_parse(name, |value| {
+        value.split(',').map(FromStr::from_str).collect()
+    })
+}
+
+fn env_var_parse<T, E>(
+    name: &'static str,
+    parse: impl FnOnce(&str) -> Result<T, E>,
+) -> anyhow::Result<Option<T>>
+where
+    E: fmt::Display,
+{
+    match std::env::var(name) {
+        Ok(value) => parse(&value)
+            .map_err(|e| anyhow!("invalid format for `{name}`: {e}"))
+            .map(Some),
+        Err(std::env::VarError::NotPresent) => Ok(None),
+        Err(std::env::VarError::NotUnicode(_)) => {
+            Err(anyhow!("invalid format for `{name}`: invalid UTF-8"))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::net::SocketAddr;
 
-    #[derive(Deserialize)]
-    struct TestConfig {
-        pub persistent_db: DatabaseConfig,
-        pub ephemeral_db: DatabaseConfig,
-    }
+    use super::load_toml;
 
     #[test]
     fn test_db() {
@@ -283,17 +431,24 @@ ephemeral_db = { path = "/1", filename = "test1" }
 persistent_db.path = "/2"
 "#;
 
-        let config = config::Config::builder()
-            .add_source(config::File::from_str(raw_config, config::FileFormat::Toml))
-            .build()
-            .unwrap();
+        let config = load_toml(Some(raw_config)).unwrap();
 
-        let db_config = config.try_deserialize::<TestConfig>().unwrap();
+        assert_eq!(config.ephemeral_db.path, "/1".to_string());
+        assert_eq!(config.ephemeral_db.filename, Some("test1".to_string()));
 
-        assert_eq!(db_config.ephemeral_db.path, "/1".to_string());
-        assert_eq!(db_config.ephemeral_db.filename, Some("test1".to_string()));
+        assert_eq!(config.persistent_db.path, "/2".to_string());
+        assert!(config.persistent_db.filename.is_none());
+    }
 
-        assert_eq!(db_config.persistent_db.path, "/2".to_string());
-        assert!(db_config.persistent_db.filename.is_none());
+    #[test]
+    // run this test as:
+    // DIOM_LISTEN_ADDRESS='0.0.0.0:1234' cargo test -p diom config_env_override -- --ignored
+    #[ignore]
+    fn test_config_env_override() {
+        let config = load_toml(None).unwrap();
+        assert_eq!(
+            config.listen_address,
+            "0.0.0.0:1234".parse::<SocketAddr>().unwrap()
+        );
     }
 }
