a3594ad6544cf1f8eef44ff49b41202c4a608574
Jonas Platte <jplatte@svix.com>
2026-02-24 15:47:21 +0100

    go: Add missing Coyote constructor function

--- clients/go/client.go
+++ clients/go/client.go
@@ -1,6 +1,7 @@
 package coyote
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"time"
@@ -17,4 +18,33 @@ type CoyoteOptions struct {
 
 type Coyote struct {
 	inner coyote_proto.HttpClient
+}
+
+func New(token string, options *CoyoteOptions) (*Coyote, error) {
+	httpClient := coyote_proto.DefaultHttpClient("http://localhost:8050")
+
+	if options != nil {
+		if options.ServerUrl != nil {
+			httpClient.BaseURL = options.ServerUrl.String()
+		}
+
+		if options.RetrySchedule != nil {
+			if len(*options.RetrySchedule) > 5 {
+				return nil, fmt.Errorf("number of retries must not exceed 5")
+			}
+			httpClient.RetrySchedule = *options.RetrySchedule
+		}
+
+		if options.HTTPClient != nil {
+			httpClient.HTTPClient = options.HTTPClient
+		}
+
+		httpClient.Debug = options.Debug
+	}
+
+	httpClient.DefaultHeaders["Authorization"] = fmt.Sprintf("Bearer %s", token)
+	httpClient.DefaultHeaders["User-Agent"] = "coyote-sdks/0.0.1/go"
+
+	client := Coyote{httpClient}
+	return &client, nil
 }
