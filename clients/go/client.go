c817aa8325e2377f72e466b673a2e4877497ea88
Jonas Platte <jplatte@svix.com>
2026-02-24 15:47:21 +0100

    go: Add missing Diom constructor function

--- clients/go/client.go
+++ clients/go/client.go
@@ -1,6 +1,7 @@
 package diom
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"time"
@@ -17,4 +18,33 @@ type DiomOptions struct {
 
 type Diom struct {
 	inner diom_proto.HttpClient
+}
+
+func New(token string, options *DiomOptions) (*Diom, error) {
+	httpClient := diom_proto.DefaultHttpClient("http://localhost:8050")
+
+	if options != nil {
+		if options.ServerUrl != nil {
+			httpClient.BaseURL = options.ServerUrl.String()
+		}
+
+		if options.RetrySchedule != nil {
+			if len(*options.RetrySchedule) > 5 {
+				return nil, fmt.Errorf("number of retries must not exceed 5")
+			}
+			httpClient.RetrySchedule = *options.RetrySchedule
+		}
+
+		if options.HTTPClient != nil {
+			httpClient.HTTPClient = options.HTTPClient
+		}
+
+		httpClient.Debug = options.Debug
+	}
+
+	httpClient.DefaultHeaders["Authorization"] = fmt.Sprintf("Bearer %s", token)
+	httpClient.DefaultHeaders["User-Agent"] = "diom-sdks/0.0.1/go"
+
+	client := Diom{httpClient}
+	return &client, nil
 }
