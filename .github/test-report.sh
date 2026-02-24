bfef68d009ac6c7648e7c1f6c1eaf8f00d3bae22
Jonas Platte <jplatte@svix.com>
2026-02-24 15:07:44 +0100

    ci: Add coverage tracking

---
+++ .github/test-report.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cargo llvm-cov nextest --profile ci --no-clean
+status=$?
+
+if [[ $status -eq 0 ]]; then
+    cargo llvm-cov report --html
+    mv target/llvm-cov/html ./coverage-html
+    coverage_percentage=$(cargo llvm-cov report --json | jq '.data[].totals.lines.percent')
+    if [[ -n "$GITHUB_OUTPUT" ]]; then
+        printf 'coverage_percent=%.2f\n' "$coverage_percentage" >> "$GITHUB_OUTPUT"
+    fi
+fi
+
+mv target/nextest/ci/junit.xml ./junit.xml
+exit $status
