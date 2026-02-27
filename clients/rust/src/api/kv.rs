914ced74d2e90735ecd1eabeb9c683564711a220
Jonas Platte <jplatte@svix.com>
2026-02-27 16:56:06 +0100

    Update Rust client to use msgpack (#243)

--- clients/rust/src/api/kv.rs
+++ clients/rust/src/api/kv.rs
@@ -13,7 +13,7 @@ impl<'a> Kv<'a> {
     /// KV Set
     pub async fn set(&self, kv_set_in: KvSetIn) -> Result<KvSetOut> {
         crate::request::Request::new(http::Method::POST, "/api/v1/kv/set")
-            .with_body_param(kv_set_in)
+            .with_body(kv_set_in)
             .execute(self.cfg)
             .await
     }
@@ -21,7 +21,7 @@ impl<'a> Kv<'a> {
     /// KV Get
     pub async fn get(&self, kv_get_in: KvGetIn) -> Result<KvGetOut> {
         crate::request::Request::new(http::Method::POST, "/api/v1/kv/get")
-            .with_body_param(kv_get_in)
+            .with_body(kv_get_in)
             .execute(self.cfg)
             .await
     }
@@ -32,7 +32,7 @@ impl<'a> Kv<'a> {
         kv_get_namespace_in: KvGetNamespaceIn,
     ) -> Result<KvGetNamespaceOut> {
         crate::request::Request::new(http::Method::POST, "/api/v1/kv/get-namespace")
-            .with_body_param(kv_get_namespace_in)
+            .with_body(kv_get_namespace_in)
             .execute(self.cfg)
             .await
     }
@@ -40,7 +40,7 @@ impl<'a> Kv<'a> {
     /// KV Delete
     pub async fn delete(&self, kv_delete_in: KvDeleteIn) -> Result<KvDeleteOut> {
         crate::request::Request::new(http::Method::POST, "/api/v1/kv/delete")
-            .with_body_param(kv_delete_in)
+            .with_body(kv_delete_in)
             .execute(self.cfg)
             .await
     }
