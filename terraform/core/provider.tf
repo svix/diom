a49b7afb2a53a119fdfbfd698a48bcbbbd433cee
Ashay <ashay.chitnis@yahoo.com>
2026-02-28 01:12:52 +0530

    feat(tf): base infra for diom

---
+++ terraform/core/provider.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.34"
+    }
+
+    kubernetes = {
+      version = "~> 3.0"
+    }
+
+    tfe = {
+      version = "~> 0.73"
+    }
+
+    tls = {
+      version = "~> 4.2"
+    }
+
+    local = {
+      version = "~> 2.6"
+    }
+
+    random = {
+      version = "~> 3.8"
+    }
+
+    # datadog = {
+    #   source  = "datadog/datadog"
+    #   version = "~> 3.86"
+    # }
+  }
+
+  backend "remote" {
+    organization = "svix-development"
+    # organization = ""
+    # organization = ""
+    workspaces {
+      prefix = "svix-diom-"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = local.tags
+  }
+}
+
+# provider "datadog" {
+#   api_key = var.datadog_api_key
+#   app_key = var.datadog_app_key
+# }
