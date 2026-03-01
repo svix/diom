08d8748abff88c870cd674fabec783dc61856be5
James Brown <jbrown@svix.com>
2026-03-01 10:27:43 -0800

    only expose option if both http1 and http2 are enabled

--- clients/cli/src/config.rs
+++ clients/cli/src/config.rs
@@ -10,7 +10,7 @@ pub struct Config {
     pub auth_token: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     server_url: Option<String>,
-    #[cfg(feature = "http1")]
+    #[cfg(all(feature = "http1", feature = "http2"))]
     #[serde(default)]
     pub http1: bool,
     #[serde(skip_serializing_if = "Option::is_none")]
