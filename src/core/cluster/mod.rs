a1e045b82ee410fafb129abc850981a4b205259a
James Lucas <svix-james@users.noreply.github.com>
2026-03-02 13:24:31 -0600

    Propagate tracing info across raft requests
    This will cause the raft requests to show up as child span of
    the original HTTP request trace.

--- src/core/cluster/mod.rs
+++ src/core/cluster/mod.rs
@@ -17,7 +17,7 @@ mod state_machine;
 
 pub use self::{
     app::router,
-    handle::{RaftState, Request},
+    handle::{RaftState, RequestWithContext},
     logs::DiomLogs,
     raft::{Raft, TypeConfig, initialize_raft},
     state_machine::Stores,
