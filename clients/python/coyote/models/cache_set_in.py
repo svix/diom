feeffc3e2e7c317ef452e0faba08686fb00d1e46
Jonas Platte <jplatte@svix.com>
2026-03-02 19:52:05 +0100

    python: Take x-positional into account in codegen (#310)

--- clients/python/coyote/models/cache_set_in.py
+++ clients/python/coyote/models/cache_set_in.py
@@ -4,6 +4,13 @@ from ..internal.base_model import BaseModel
 
 
 class CacheSetIn(BaseModel):
+    value: bytes
+
+    ttl: int
+    """Time to live in milliseconds"""
+
+
+class _CacheSetIn(BaseModel):
     key: str
 
     value: bytes
