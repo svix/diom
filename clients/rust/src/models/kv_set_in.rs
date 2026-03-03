180a8122e90b2b3b8be1aa91f24296bd3ff8a523
Jonas Platte <jplatte@svix.com>
2026-03-03 16:28:37 +0100

    Reorder fields in KvSetIn (#329)

--- clients/rust/src/models/kv_set_in.rs
+++ clients/rust/src/models/kv_set_in.rs
@@ -7,23 +7,23 @@ use super::operation_behavior::OperationBehavior;
 pub struct KvSetIn {
     pub key: String,
 
+    pub value: Vec<u8>,
+
     /// Time to live in milliseconds
     #[serde(skip_serializing_if = "Option::is_none")]
     pub ttl: Option<u64>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub behavior: Option<OperationBehavior>,
-
-    pub value: Vec<u8>,
 }
 
 impl KvSetIn {
     pub fn new(key: String, value: Vec<u8>) -> Self {
         Self {
             key,
+            value,
             ttl: None,
             behavior: None,
-            value,
         }
     }
 }
