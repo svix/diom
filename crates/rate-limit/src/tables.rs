25969f44daecdd94faeb7b7dfa657236099cc396
Tom Hacohen <tasn@users.noreply.github.com>
2026-03-11 14:23:40 -0400

    Rate-limit: remove 'key' field from value. (#449)
    We did it in other modules as well, it's not actually needed in the
    body.

--- crates/rate-limit/src/tables.rs
+++ crates/rate-limit/src/tables.rs
@@ -11,7 +11,6 @@ enum RowType {
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct FixedWindowState {
-    pub key: String,
     pub count: u64,
     pub window_start: Timestamp,
 }
@@ -28,7 +27,6 @@ impl FixedWindowState {
 
 #[derive(Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct TokenBucketState {
-    pub key: String,
     pub tokens: u64,
     pub last_refill: Timestamp,
 }
