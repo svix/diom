88e5b0dee5bb2b44013ea9fad2fc2be3f454c7db
James Brown <jbrown@svix.com>
2026-03-11 08:16:54 -0700

    move monotime to coyote-core and add to AppState (#438)

--- crates/coyote-core/src/lib.rs
+++ crates/coyote-core/src/lib.rs
@@ -1,6 +1,9 @@
 use std::sync::LazyLock;
 
+mod monotime;
 pub mod shutdown;
 pub mod types;
 
 pub static INSTANCE_ID: LazyLock<String> = LazyLock::new(|| uuid::Uuid::new_v4().to_string());
+
+pub use monotime::Monotime;
