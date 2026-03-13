ae2601768288acf6837704a6bc74d4f06b91ea4a
James Brown <jbrown@svix.com>
2026-03-13 10:01:22 -0700

    resolve merge issues

--- src/core/cluster/handle.rs
+++ src/core/cluster/handle.rs
@@ -397,7 +397,7 @@ impl coyote_operations::OperationWriterBase for RaftState {
                 } else {
                     tracing::warn!(?err, "unhandled error writing request to raft");
                     Err(coyote_operations::BackgroundError::Other(
-                        coyote_error::Error::generic(err),
+                        coyote_error::Error::internal(err),
                     ))
                 }
             }
