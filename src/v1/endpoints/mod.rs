5089c3880eec87c1cbf749a686d208868859bbbb
Tom Hacohen <tom@stosb.com>
2026-03-19 15:46:22 -0700

    Auth: add a basic auth middleware and admin tokens.
    Admin tokens are essentially just hardcoded tokens people can set in
    configs or environment variables.
    They are mostly useful in CI and shouldn't be used under normal
    operations.
    At the moment they can also be used to bootstrap a server, but we'll add
    better mechanisms in the future.
    
    This also adds a basic auth middleware that validates the admin token
    and injects `Permissions` into the axum handler.

--- src/v1/endpoints/mod.rs
+++ src/v1/endpoints/mod.rs
@@ -1,8 +1,5 @@
 // SPDX-FileCopyrightText: © 2022 Svix Authors
 // SPDX-License-Identifier: MIT
-use aide::axum::ApiRouter;
-
-use crate::AppState;
 
 pub mod admin;
 pub mod cache;
@@ -12,15 +9,3 @@ pub mod kv;
 pub mod msgs;
 pub mod queue;
 pub mod rate_limit;
-
-pub fn router() -> ApiRouter<AppState> {
-    ApiRouter::new()
-        .merge(admin::router())
-        .merge(health::router())
-        .merge(cache::router())
-        .merge(kv::router())
-        .merge(rate_limit::router())
-        .merge(idempotency::router())
-        .merge(queue::router())
-        .merge(msgs::router())
-}
