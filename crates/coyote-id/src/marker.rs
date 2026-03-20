cb5f73a0827bed03ff9d241b67be789cb27db5ee
Jonas Platte <jplatte@svix.com>
2026-03-20 16:59:12 +0100

    id: Make ID types more convenient to use (#543)
    … and implement JsonSchema for Public<Id>.
    
    No longer need to wrap public IDs in `Internal<_>` to get the compact format,
    it will be the default – but it still won't be possible to use it in public
    interfaces because we derive `JsonSchema` for all the public interface types,
    and that will fail for ID usage without `Public<_>`.

--- crates/coyote-id/src/marker.rs
+++ crates/coyote-id/src/marker.rs
@@ -1,37 +1,19 @@
-pub trait IdMarker {
-    type Use;
-}
+pub trait IdMarker {}
 
-/// Marker type for internal-only ID markers.
-///
-/// (yes, the marker-ception serves a purpose)
-pub enum InternalUse {}
-
-/// Marker type for pubically-used ID markers.
-///
-/// (yes, the marker-ception serves a purpose)
-pub enum PublicUse {}
-
-pub trait PublicIdMarker: IdMarker<Use = PublicUse> {
+pub trait PublicIdMarker: IdMarker {
     const PREFIX: &str;
 }
 
 macro_rules! id_marker {
-    ( $name:ident $(,)? ) => {
-        id_marker!(@impl $name, InternalUse);
-    };
-    ( $name:ident, $prefix:literal $(,)? ) => {
-        id_marker!(@impl $name, PublicUse);
-
-        impl $crate::marker::PublicIdMarker for $name {
-            const PREFIX: &str = $prefix;
-        }
-    };
-    ( @impl $name:ident, $use:ident ) => {
+    ( $name:ident $(, $prefix:literal)? $(,)? ) => {
         pub enum $name {}
 
-        impl $crate::marker::IdMarker for $name {
-            type Use = $crate::marker::$use;
-        }
-    }
+        impl $crate::marker::IdMarker for $name {}
+
+        $(
+            impl $crate::marker::PublicIdMarker for $name {
+                const PREFIX: &str = $prefix;
+            }
+        )?
+    };
 }
