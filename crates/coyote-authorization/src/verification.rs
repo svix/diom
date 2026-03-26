3cfa0666c972587d1d46e17f20fe0f3a2ee21db9
Jonas Platte <jplatte@svix.com>
2026-03-26 19:05:33 +0100

    Add verify_operation for checking an operation against a list of `AccessRule`s (#664)

---
+++ crates/coyote-authorization/src/verification.rs
@@ -0,0 +1,31 @@
+use crate::{AccessRule, AccessRuleEffect, RequestedOperation};
+
+pub struct Forbidden;
+
+pub fn verify_operation(
+    operation: &RequestedOperation<'_>,
+    rules: &[AccessRule],
+) -> Result<(), Forbidden> {
+    // Fallback if no rule matches: reject
+    let mut result = Err(Forbidden);
+
+    for rule in rules {
+        if rule.matches(operation) {
+            match rule.effect {
+                AccessRuleEffect::Allow => {
+                    // found an allow rule, so set the result accordingly.
+                    // still need to go through other rules, as any deny rule
+                    // that also matches takes precedence.
+                    result = Ok(());
+                }
+                AccessRuleEffect::Deny => {
+                    // deny rules take precedence, if we found a matching one
+                    // we can stop going through the rest and reject.
+                    return Err(Forbidden);
+                }
+            }
+        }
+    }
+
+    result
+}
