431d0130912ba3ddd9a0cbe57c7e3eab0bf5ca31
svix-lucho <133019767+svix-lucho@users.noreply.github.com>
2026-03-30 10:42:40 -0300

    Bundle JS library with tsdown (#694)
    Add [tsdown](https://github.com/rolldown/tsdown/) to bundle the JS/TS
    lib for ESM Node and browsers.
    
    I decided to use a bundler instead of using plain `tsc` because `tsc`
    emits `.js` files and keeps extensionless import `./example`. Node’s ESM
    loader won’t resolve those paths, so the only way to make that work for
    Node was to add `.js` extensions to all imports in the source files,
    which seemed very ugly.
    
    `tsdown` seems very simple, and we can configure it to bundle the lib
    for whatever target we want to quite easily. It's not webpack/vite
    config hell. And it's built on top of [Rolldown](https://rolldown.rs/),
    a bundler written in Rust.
    
    This also cleans up test related code that was unused. When we add tests
    here, we can add it back.

--- z-clients/javascript/src/index.ts
+++ z-clients/javascript/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./client";
-export { CoyoteOptions } from "./options";
+export type { CoyoteOptions } from "./options";
 export { ApiException } from "./util";
 export { HTTPValidationError, HttpErrorOut, ValidationError } from "./HttpErrors";
