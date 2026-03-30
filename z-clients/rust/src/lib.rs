a82ac7d186c552cdcf5ba9e071a2d8a939e3e432
Jonas Platte <jplatte@svix.com>
2026-03-30 17:20:41 +0200

    rust: Clean up client configuration (#707)

--- z-clients/rust/src/lib.rs
+++ z-clients/rust/src/lib.rs
@@ -1,8 +1,3 @@
-use std::time::Duration;
-
-use hyper::body::Bytes;
-use hyper_util::client::legacy::Client as HyperClient;
-
 pub mod api;
 mod client;
 mod connector;
@@ -10,19 +5,8 @@ mod error;
 pub mod models;
 mod request;
 
-use self::connector::Connector;
+pub(crate) use self::client::Configuration;
 pub use self::{
     client::{DiomClient, DiomOptions, DEFAULT_URL},
     error::{Error, Result},
 };
-
-pub struct Configuration {
-    pub base_path: String,
-    pub user_agent: Option<String>,
-    pub bearer_access_token: Option<String>,
-    pub timeout: Option<Duration>,
-    pub num_retries: u32,
-    pub retry_schedule: Option<Vec<Duration>>,
-
-    client: HyperClient<Connector, http_body_util::Full<Bytes>>,
-}
