a5c3ecfdd846db2562926d8c87b1b1c86df58c9c
Pedro Luiz <pluiz@svix.com>
2026-03-31 09:22:14 -0300

    update SCRIPT_DIR and ROOT_DIR definition

--- scripts/run-sdk-integration-tests.sh
+++ scripts/run-sdk-integration-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+SCRIPT_DIR="$(dirname "$0")"
+ROOT_DIR="$SCRIPT_DIR/.."
 
 COYOTE_TOKEN="admin_abcdefghijlmnopqrstuvwxyz012345"
 CONTAINER_NAME="coyote-test"
