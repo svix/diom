d35f32d70a115d7472fea0997c5a26a57276ca7b
Pedro Luiz <pluiz@svix.com>
2026-04-03 18:19:36 -0300

    fix(codegen): import java.util.List

--- z-clients/java/src/main/java/com/svix/coyote/apis/Idempotency.java
+++ z-clients/java/src/main/java/com/svix/coyote/apis/Idempotency.java
@@ -9,6 +9,7 @@ import com.svix.coyote.Utils;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import lombok.Getter;
