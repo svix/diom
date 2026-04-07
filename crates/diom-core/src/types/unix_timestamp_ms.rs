73ba18ed4397fbaaa9a63dcb598c1cd0f37eca14
Jonas Platte <jplatte@svix.com>
2026-04-07 16:16:25 +0200

    backend: Change timestamp representation to milliseconds since unix epoch

---
+++ crates/diom-core/src/types/unix_timestamp_ms.rs
@@ -0,0 +1,74 @@
+use std::{borrow::Cow, fmt};
+
+use schemars::{JsonSchema, Schema, json_schema};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnixTimestampMs(u64);
+
+impl UnixTimestampMs {
+    fn new(millis: u64) -> Result<Self, &'static str> {
+        if millis > jiff::Timestamp::MAX.as_millisecond() as u64 {
+            return Err("timestamp too large");
+        }
+        Ok(Self(millis))
+    }
+}
+
+impl fmt::Debug for UnixTimestampMs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let ts = jiff::Timestamp::from_millisecond(
+            self.0
+                .try_into()
+                .expect("timestamps beyond year 9999 not supported"),
+        );
+        ts.fmt(f)
+    }
+}
+
+impl From<jiff::Timestamp> for UnixTimestampMs {
+    #[track_caller]
+    fn from(value: jiff::Timestamp) -> Self {
+        let value = u64::try_from(value.as_millisecond())
+            .expect("timestamps in diom must never be before the unix epoch");
+        Self(value)
+    }
+}
+
+impl Serialize for UnixTimestampMs {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.0.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for UnixTimestampMs {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let millis = u64::deserialize(deserializer)?;
+        Self::new(millis).map_err(serde::de::Error::custom)
+    }
+}
+
+impl JsonSchema for UnixTimestampMs {
+    fn schema_name() -> Cow<'static, str> {
+        "UnixTimestampMs".into()
+    }
+
+    fn json_schema(_: &mut schemars::SchemaGenerator) -> Schema {
+        json_schema!({
+            "type": "integer",
+            "format": "uint64",
+            "minimum": 0,
+            "x-subtype": "UnixTimestampMs",
+        })
+    }
+
+    fn inline_schema() -> bool {
+        true
+    }
+}
