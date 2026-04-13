96035a887640fe05da07a3598863e72892c02eab
Jonas Platte <jplatte@svix.com>
2026-04-13 15:47:08 +0200

    Appease clippy

--- crates/test-utils/src/time.rs
+++ crates/test-utils/src/time.rs
@@ -3,17 +3,17 @@ use std::time::Duration;
 use jiff::Timestamp;
 
 pub trait U64Ext {
-    fn as_duration_ms(self) -> Duration;
-    fn as_timestamp_ms(self) -> Timestamp;
+    fn as_duration_ms(&self) -> Duration;
+    fn as_timestamp_ms(&self) -> Timestamp;
 }
 
 impl U64Ext for u64 {
-    fn as_duration_ms(self) -> Duration {
-        Duration::from_millis(self)
+    fn as_duration_ms(&self) -> Duration {
+        Duration::from_millis(*self)
     }
 
     #[track_caller]
-    fn as_timestamp_ms(self) -> Timestamp {
-        Timestamp::from_millisecond(self.try_into().unwrap()).unwrap()
+    fn as_timestamp_ms(&self) -> Timestamp {
+        Timestamp::from_millisecond((*self).try_into().unwrap()).unwrap()
     }
 }
