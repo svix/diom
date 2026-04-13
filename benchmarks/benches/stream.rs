f0ad19f49cdf1f3d821ca851bf4c92890f1c5717
Jonas Platte <jplatte@svix.com>
2026-04-13 13:41:01 +0200

    Upgrade rand to 0.10 (#943)

--- benchmarks/benches/stream.rs
+++ benchmarks/benches/stream.rs
@@ -5,7 +5,7 @@ use diom::models::{
     MsgIn, MsgNamespaceCreateIn, MsgPublishIn, MsgStreamCommitIn, MsgStreamReceiveIn,
 };
 use diom_benchmarks::{BenchmarkContext, setup_cluster, setup_single_server};
-use rand::{Rng, SeedableRng, rngs::StdRng};
+use rand::{RngExt, SeedableRng, rngs::StdRng};
 
 fn bench_stream<'a, M: Measurement>(ctx: BenchmarkContext, group: &mut BenchmarkGroup<'a, M>) {
     let client = ctx.client;
