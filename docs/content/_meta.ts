7f51de375f7ae472fd56975536032d3534fc7702
James Lucas <svix-james@users.noreply.github.com>
2026-04-13 11:46:23 -0500

    Remove docs and svix-specific deploy configs
    Also deploys the on-main Docker publish steps to private repos.
    
    For now, Helm step has not been removed. Publishing it to a private
    Helm repository will take a bit more work.

--- docs/content/_meta.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  index: 'Introduction',
-  quickstart: 'Quickstart',
-  namespaces: 'Namespaces',
-  'access-control': 'Access Control',
-  modules: 'Modules',
-  'self-hosting': 'Self-Hosting',
-}
