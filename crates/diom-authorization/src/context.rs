173f5bff8f6722377810dc7e58eaa69ba0e3e51d
Jonas Platte <jplatte@svix.com>
2026-04-16 13:12:57 +0200

    Extract constant in authorization/context.rs

--- crates/diom-authorization/src/context.rs
+++ crates/diom-authorization/src/context.rs
@@ -2,6 +2,8 @@ use std::{collections::HashMap, sync::LazyLock};
 
 use crate::Permissions;
 
+const USER_CONTEXT_PREFIX: &str = "context.";
+
 #[derive(Clone, Copy)]
 pub struct Context<'a> {
     pub role: &'a str,
@@ -28,7 +30,7 @@ impl<'a> Context<'a> {
     pub fn get(&self, key: &str) -> Option<&str> {
         if key == "role" {
             Some(self.role)
-        } else if let Some(name) = key.strip_prefix("context.") {
+        } else if let Some(name) = key.strip_prefix(USER_CONTEXT_PREFIX) {
             self.map.get(name).map(|s| s.as_str())
         } else {
             None
