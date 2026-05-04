2c499c0cdb5b61bca45c349bee32800a78b48732
Jonas Platte <jplatte@svix.com>
2026-05-04 19:11:04 +0200

    js: Revamp error types

--- z-clients/javascript/src/index.ts
+++ z-clients/javascript/src/index.ts
@@ -1,4 +1,10 @@
 export * from "./client";
 export type { DiomOptions } from "./options";
-export { ApiException } from "./util";
-export { HTTPValidationError, HttpErrorOut, ValidationError } from "./HttpErrors";
+export {
+  ConnectionError,
+  DiomError,
+  InvalidInput,
+  OperationError,
+  OtherError,
+  ServerError,
+} from "./error";
