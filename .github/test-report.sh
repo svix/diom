66c540973fc7ad9db39f018c28f08053dccf597a
Jonas Platte <jplatte@svix.com>
2026-09-02 12:15:29 +0200

    ci: Split PR comment actions into separate workflow
    This should allow CI to succeed on PRs from forks.

--- .github/test-report.sh
+++ .github/test-report.sh
@@ -7,12 +7,9 @@ cargo llvm-cov nextest --profile ci --no-clean
 status=$?
 
 if [[ $status -eq 0 ]]; then
+    cargo llvm-cov report --json >coverage.json
     cargo llvm-cov report --html
     mv target/llvm-cov/html ./coverage-html
-    coverage_percentage=$(cargo llvm-cov report --json | jq '.data[].totals.lines.percent')
-    if [[ -n "$GITHUB_OUTPUT" ]]; then
-        printf 'coverage_percent=%.2f\n' "$coverage_percentage" >>"$GITHUB_OUTPUT"
-    fi
 fi
 
 mv target/nextest/ci/junit.xml ./junit.xml
